2eb648bd577bfd46825d973886f99c92e3143d26
zdenekvesely <zd.vesely@gmail.com>
2020-01-16 13:56:22 +0100

    i001: data description corrected

--- R/datasets.R
+++ R/datasets.R
@@ -1,12 +1,12 @@
 #' FVE production in Czech republic and daily average temperature in Prague
 #'
 #' A dataset containing the FVE production in Czech republic and daily average
-#' temperature in Prague, years 2010 - 2019
+#' temperature in Prague, years 2010 - 2018
 #'
 #' @format A data frame with 3287 rows and 3 variables:
 #' \describe{
 #'   \item{date}{datum}
-#'   \item{temperature}{daily avearage temperature, in °C}
+#'   \item{temperature}{daily average temperature, in °C}
 #'   \item{FVE}{daily production of FVE, in MWh}
 #' }
 #' @source \url{https://www.ceps.cz/cs/data#Generation},
